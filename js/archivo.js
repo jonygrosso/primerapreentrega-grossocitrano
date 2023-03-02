ebcf937b06ec2b2ff45e80b163cf00175f0f2fe8
jonygrosso <jonathangrosso@hotmail.com>
2023-03-02 18:17:17 -0300

    Tercera Entrega

--- js/archivo.js
+++ js/archivo.js
@@ -1,113 +1,102 @@
-//VARIABLES GLOBALES
-let nombre;
-let apellido;
-let dni;
-let importe;
-let cuotas;
-let x = 0;
-let i = 0;
+// VARIABLES GLOBALES
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let dni = document.getElementById("dni");
+let importe = document.getElementById("importe");
+let cuotas = document.getElementById("cuotas");
+let botonaceptar = document.getElementById("botonaceptar");
+let botonmostrar = document.getElementById("botonmostrar");
+let botonborrar = document.getElementById("botonborrar");
+let valorporcuota= this.calcularPrestamo;
+// ARREGLO DE CLIENTE
+let lista_clientes = [];
 
-//CLASE CLIENTE CON DATOS BASICOS
+// CLASE CLIENTE CON DATOS BASICOS
 class Cliente {
-    constructor(nombre, apellido, dni,edad){
-    this.nombre=nombre;
-    this.apellido=apellido;
-    this.dni=dni;
-    this.edad=edad; 
-
-    }
-}
-//ARREGLO DE CLIENTE
-let lista_clientes =[];
-
-//FUNCION QUE HACE EL CALCULO DEL PRESTAMO Y MUESTRA VALOR DE CUOTAS Y DEMAS
-function prestamo (importe, cuotas){
-    importe= parseFloat(importe);
-    cuotas= parseInt(cuotas);
-
-    let prestamo = 0;
-
-    if (importe > 0 && (cuotas >0 || cuotas <=3)){
-        prestamo=importe+(importe*0.13);
-        console.log ("Pediste ",importe,)
-        console.log ("Vas a terminar devolviendo: " ,prestamo);
-        console.log ("En ", cuotas ," cuotas.");
-        console.log ("El valor de cada una es: ",(prestamo/cuotas));
-        console.log ("-----------------------------------------------");
-        console.log ("-----------------------------------------------");
-
-
-    }
-    else if (importe > 0 && (cuotas >3 || cuotas <=6) ){
-        prestamo=importe+(importe*0.35);
-        console.log ("Pediste ",importe,)
-        console.log ("Vas a terminar devolviendo: " ,prestamo);
-        console.log ("En ", cuotas ," cuotas.");
-        console.log ("El valor de cada una es: ",(prestamo/cuotas));
-        console.log ("-----------------------------------------------");
-        console.log ("-----------------------------------------------");
-    }
-    else if (importe > 0 && (cuotas > 6 || cuotas <=12) ){
-        prestamo=importe+(importe*0.45);
-        console.log ("Pediste ",importe,)
-        console.log ("Vas a terminar devolviendo: " ,prestamo);
-        console.log ("En ", cuotas ," cuotas.");  
-        console.log ("El valor de cada una es: ",(prestamo/cuotas));
-        console.log ("-----------------------------------------------");
-        console.log ("-----------------------------------------------");
-
-    }
-    else  {
-    console.log ("PORFAVOR INGRESE IMPORTE O CANTIDAD DE CUOTAS REALES");
-    }
-
-
-}
-
-
-
-console.log ("Bienvenidos a Prestamos El Regalo ");
-//BUCLE FOR REALIZA EL CICLO CUANTOS CLIENTES SE NECESITE SIMULAR PRESTAMO 
-for (i; i==x; i=i+1 ) {
-
-    let edad=prompt ("Ingrese su edad para validarse en el sistema: ");
-    let bol=true;
-
-// VALIDA E INGRESA AL BUCLE SI ES MAYOR DE EDAD Y VALIDA CON UNA VARIABLE BOOLEANA (BOL)
-    while (edad>=18 && bol == true){
-    nombre = prompt ("Ingrese su nombre: ");
-    apellido = prompt ("Ingrese su apellido: ");
-    dni = prompt ("Ingrese su DNI: ");
-    let cliente1 = new Cliente (nombre, apellido, dni, edad);
-    lista_clientes.push(cliente1);
-
-
-    importe= prompt("Ingrese el Monto del prestamo a solicitar: ");
-    cuotas= prompt("Ingrese la cantidad de cuotas entre 1 y 12: ");
-    console.log (cliente1.nombre," ",cliente1.apellido);
-    prestamo(importe, cuotas);
-
-
-    bol=false;
-        
-    }
-        if (edad < 18){
-    alert("EDAD INVALIDA");
+  constructor(nombre, apellido, dni, edad, importe, cuotas) {
+    this.nombre = nombre.value;
+    this.apellido = apellido.value;
+    this.dni = dni.value;
+    this.edad = edad.value;
+    this.importe = importe.value;
+    this.cuotas = cuotas.value;
+    this.prestamo = this.calcularPrestamo();
+    this.valorporcuota= valorporcuota;
     
-    }
- //CONDICIONAL PARA VOLVER AL BUCLE FOR 
-let A = prompt("Desea agregar otra simulacion con otro cliente nuevo (Y para si N para no): ");
-if (A == "Y" || A== "y"){
-    x=x+1;
+  }
+//FUNCION QUE REALIZA EL CALCULO
+  calcularPrestamo() {
+    let importe = parseFloat(this.importe);
+    let cuotas = parseInt(this.cuotas);
+    let prestamo = 0;
+    valorporcuota=parseFloat(valorporcuota);
+
+    if (importe > 0 && cuotas > 0 && cuotas <= 3) {
+      prestamo = importe + (importe * 0.13);
+      valorporcuota=(prestamo/cuotas).toFixed(2);
+    } else if (importe > 0 && cuotas > 3 && cuotas <= 6) {
+      prestamo = importe + (importe * 0.35);
+      valorporcuota=(prestamo/cuotas).toFixed(2)
+    } else if (importe > 0 && cuotas > 6 && cuotas <= 12) {
+      prestamo = importe + (importe * 0.45);
+      valorporcuota=(prestamo/cuotas).toFixed(2);
+    } 
+
+    return prestamo;
+    
+  }
 }
-else if (A == "N" || A=="n"){
-    x=i;
+
+
+
+
+function mostrar() {
+  let fila = document.getElementById("Mostrar");
+  fila.innerHTML="";  
+for (let cliente_uno of lista_clientes){
+  let h =document.createElement("h3");
+  h.innerHTML = `<h5>El Cliente: ${cliente_uno.nombre} ${cliente_uno.apellido} pidio $${cliente_uno.importe}
+   en ${cliente_uno.cuotas} cuotas.</h5>
+
+   <h5>Termina pagando: $${cliente_uno.prestamo} el valor de la cuota seria de $${cliente_uno.valorporcuota}</h5>
+   <button class="btn_borrar" id="botonborrar">Borrar</button>
+   <h3>-------------------------------------------------</h3>`;
+  fila.append(h);
 }
-else {
-    alert ("Error Ingrese lo que se pidio")
-} 
- 
 }
+
+function aceptar() {
+  let cliente1 = new Cliente(nombre, apellido, dni, edad, importe, cuotas);
+  lista_clientes.push(cliente1);
+  nombre.value="";
+  apellido.value="";
+  dni.value="";
+  edad.value="";
+  importe.value="";
+  cuotas.value="";  
+  nombre.focus();
+  //Almacenamiento de Storage
+  /*Segun lo que dijo el profe en la clase de repaso, que no le podemos dar
+  utilidad al almacenamiento en storage al ser algo tan simple*/
+  let cliente_JSON = JSON.stringify(lista_clientes);
+  localStorage.setItem("clientes " , cliente_JSON );
+  let recuperando_cliente = localStorage.getItem("clientes");
+  recuperando_cliente = JSON.parse( recuperando_cliente );
+
+}
+//EVENTO CLICK BOTONES
+
+botonaceptar.addEventListener("click",aceptar);
+botonmostrar.addEventListener("click", mostrar);
+
+
+
+
+
+
+
+
+
 
  //APARTADO SOLO PARA EL ADMINISTRADOR DEL SISTEMA
 
